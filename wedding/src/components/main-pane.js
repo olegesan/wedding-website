b356b6235b734cd65e765a557dcefcb3a3b54a03
olegesan <37033572+olegesan@users.noreply.github.com>
2020-03-14 16:45:46 -0400

    added text to the main pane, 2 sizes

--- wedding/src/components/main-pane.js
+++ wedding/src/components/main-pane.js
@@ -18,17 +18,18 @@ const properties = {
 class MainPane extends Component{
     
     render(){
+        const slides = SlideImages.map((img, key)=>{
+            return (
+                <div className={'each-slide'}>
+                    <div key={key} className={'background-tint'} style={{'backgroundImage': `url(${img})`}}>
+                        <span>We're Engaged!</span>
+                    </div>
+                </div>
+            )
+        })
         return(
             <Slide {...properties} >
-
-                <div className='each-slide'>
-                    <div className={'background-tint'} style={{'backgroundImage': `url(${SlideImages[0]})`}}>
-                    </div>
-                </div><div className='each-slide'>
-                    <div className={'background-tint'} style={{'backgroundImage': `url(${(SlideImages[1])})`}}></div>
-                </div><div className='each-slide'>
-                    <div className='background-tint' style={{'backgroundImage': `url(${SlideImages[2]})`}}></div>
-                </div>
+                {slides}
             </Slide>
             )
     }
