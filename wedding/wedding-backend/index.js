1278a77f217c09bc604cd00ad4e2add7b2664447
olegesan <37033572+olegesan@users.noreply.github.com>
2020-03-13 22:33:51 -0400

    added index.js for backend server

---
+++ wedding/wedding-backend/index.js
@@ -0,0 +1,14 @@
+const   express = require('express'),
+        cors = require('cors'),
+        bodyParser = require('body-parser'),
+        app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send("hello")
+})
+app.listen(port, ()=>{
+    console.log("Server is running on "+port);
+})
