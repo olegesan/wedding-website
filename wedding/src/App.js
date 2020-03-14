c4f21b62bc250503a3b45ff141073f5fe6a64ad2
rubencristea <rubendcristea@gmail.com>
2020-03-14 00:11:47 -0400

    Made navbar pretty. Does not work for smaller screen sizes yet

--- wedding/src/App.js
+++ wedding/src/App.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './components/Header'
 
+function Head() {
+  return (
+    <html>
+      <head>
+        <style>
+          <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet"/>
+        </style>
+      </head>
+    </html>
+  )
+}
+
 class App extends Component {
   handleEnter = (e) => {
       this.setState({
@@ -12,10 +24,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Head></Head>
         <Header></Header>
+        <body></body>
       </div>
     );
-  }
+  }  
 }
 
 export default App;
