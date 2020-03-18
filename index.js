8564f3663224b0ba48e84fca7630fb0516d9dd43
olegesan <37033572+olegesan@users.noreply.github.com>
2020-03-17 22:32:40 -0400

    changed port to 5000

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const   express = require('express'),
         email = require('./routes/email.js');
         path = require('path')
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
